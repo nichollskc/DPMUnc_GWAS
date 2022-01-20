3aac974e35eabbade70ef341c2eb6a2215d9a214
Kath Nicholls <nichollskc@gmail.com>
2022-01-20 16:41:18 +0000

    Prepare data correctly - neaten up

--- prepare_data.R
+++ prepare_data.R
@@ -1,21 +1,35 @@
-data = readRDS("Kath_clustering_data_v3.RDS")
-data$Var.Delta = as.numeric(data$Var.Delta)
+setwd("docs/PhD/UncertaintyClustering/Burren/")
 
-save_subset <- function(rows_to_keep, dir_name) {
-    delta = reshape(data[rows_to_keep, c("PC", "Delta", "Label")], idvar="Label", timevar = "PC", direction = "wide")
-    delta_var = reshape(data[rows_to_keep, c("PC", "Var.Delta", "Label")], idvar="Label", timevar = "PC", direction = "wide")
-    
-    dir.create(paste0("data/", dir_name))
-    write.table(delta, file=paste0("data/", dir_name, "/beta.tsv"), sep='\t', row.names=FALSE)
-    write.table(delta_var, file=paste0("data/", dir_name, "/var.tsv"), sep='\t', row.names=FALSE)
+save_subset <- function(rows_to_keep, dir_name) {                                                                          
+  delta = reshape(df[rows_to_keep, c("PC", "delta", "label")], idvar="label", timevar = "PC", direction = "wide")
+  delta_var = reshape(df[rows_to_keep, c("PC", "var.delta", "label")], idvar="label", timevar = "PC", direction = "wide")
+
+  dir.create(paste0("data/", dir_name), recursive = TRUE)
+  write.table(delta, file=paste0("data/", dir_name, "/beta.tsv"), sep='\t', row.names=FALSE)
+  write.table(delta_var, file=paste0("data/", dir_name, "/var.tsv"), sep='\t', row.names=FALSE)
 }
 
-is_myositis = data$First_Author %in% c("Miller", "Rothwell") | data$Label == "Dermatopolymyositis / FinnGen" | data$Label == "Polymyositis / FinnGen"
-excluding_combined = (data$Label != "Myositis (European) / Miller")
+df <- read.table("13073_2020_797_MOESM6_ESM.csv", skip=1, sep=',', header=TRUE) %>%
+  mutate(fdr.overall.below.thresh = fdr.overall < 0.01,
+         label = paste(category.label, trait.label))
 
-save_subset(excluding_combined, "myositis_plus_imd_76")
+# Look for combined traits - we will ignore these and use only their subtypes
+is_combined_trait = grepl("comb", df$trait.label, ignore.case=TRUE)
 
-is_myositis_not_combined = is_myositis & excluding_combined
-save_subset(is_myositis_not_combined, "myositis_11")
+# Traits which are subtypes of these combined traits
+is_from_combined_trait_category = df$category.label %in% df[is_combined_trait, "category.label"]
+is_subtype = (is_from_combined_trait_category & !is_combined_trait)
 
-save_subset(!is_myositis, "imd_65")
+# Traits listed in Figure 5 of Astle - probably a sensible subset
+# We can also define this subset as below, by excluding any traits that are combinations of other traits
+from_astle_fig_5 = c("plt","mpv","pdw","pct","rbc","mcv","hct","mch","mchc","hgb",
+                     "rdw","ret","irf","mono","neut","eo","baso","gran","lymph")
+
+# discard any that are percentages and sums
+# myeloid and wbc are sums, just with nice names
+# hlr (high light scatter reticulocytes) is related to irf (immature reticulocyte fraction) so keep just one
+is_complex_astle_trait = df$category.label == "ASTLE" & grepl("_p$|_p_|_sum|myeloid|wbc|hlr", df$trait.label)
+
+is_simple_below_thresh = (df$fdr.overall.below.thresh & !is_complex_astle_trait & !is_combined_trait)
+keep_dataset <- is_subtype | is_simple_below_thresh
+save_subset(keep_dataset, "with_subtypes_001")
